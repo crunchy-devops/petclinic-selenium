e14743f3294c38314b42c9a443447e89cec70f14
Herve <dockerlite@gmail.com>
2024-02-18 16:36:22 +0100

    test revoir

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -30,7 +30,7 @@ public class test {
     public void searchString() {
         // find search box and enter search string
         String pagecontent = driver.getPageSource();
-        String search="bonjour";
+        String search="revoir";
         if (pagecontent.toLowerCase().indexOf(search.toLowerCase()) != -1) {
             System.out.println("OK");
         }else {
