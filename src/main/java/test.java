cd208fcbb5ce70630b571e1e1e3580069d2e8fcc
Herve <dockerlite@gmail.com>
2024-03-19 11:20:28 +0100

    changing test to bonjour

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -30,7 +30,7 @@ public class test {
     public void searchString() {
         // find search box and enter search string
         String pagecontent = driver.getPageSource();
-        String search="revoir";
+        String search="bonjour";
         if (pagecontent.toLowerCase().indexOf(search.toLowerCase()) != -1) {
             System.out.println("OK");
         }else {
