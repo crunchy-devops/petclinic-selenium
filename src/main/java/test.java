62f074ee863eb5f04ebf12ec200d6ec66d15b9c7
Herve <dockerlite@gmail.com>
2024-03-19 13:39:22 +0100

    changing test to bonjour

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
